1ff554ab5e263810759b30d25b858ae92249d4dc
Colby <colbyreinhart@gmail.com>
2021-03-15 16:39:14 -0400

    Coded C++ palindrome checker

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # SPL-Coding-Project
 This repo contains all associated materials for the Structure of Programming Languages Coding Project
-
 Programming languages used: C++ and Python
+
+C++ compilation instructions
+compiler: g++
+commands: g++ palindromeChecker.cpp
+		  run produced executable
+
+Python compilation instructions
---
+++ palindromeChecker.cpp
@@ -0,0 +1,33 @@
+// Palindrome Checker
+// Structure of Programming Languages - Coding Project
+// Written by Colby Reinhart
+// 3-15-2021
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+
+	// Take input from user
+	std::cout << "Enter word to check: ";
+	std::string userInput;
+	std::cin >> userInput;
+
+	// Check if palindrome
+	bool isPalindrome = true;
+	int start = 0, end = userInput.size() - 1;
+
+	while (start < end)
+	{
+		if (userInput[start] != userInput[end])
+		{
+			isPalindrome = false;
+			break;
+		}
+		++start; --end;
+	}
+
+	std::cout << userInput << " is " << (!isPalindrome ? "not " : "") << "a palindrome." << std::endl;
+
+}
